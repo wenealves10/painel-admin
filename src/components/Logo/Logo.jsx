004045dde80a239d826452edcaa9ad0bf722f344
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-15 10:21:00 -0300

    Components Logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Logo.css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => 
+    (<aside className="logo">
+        Logo
+    </aside>)
