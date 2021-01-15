9798aaa11a72334f6d3f7bc34c60029c53d9db70
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-15 12:01:39 -0300

    Nav Components

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import './Nav.css';
-
+import NavItem from '../Nav-Item/NavItem';
 
 export default props => (
     <aside className="menu-area">
-        Nav
+        <nav className="menu">
+        <NavItem icon="home" link="#/" label="Inicío"/>
+        <NavItem icon="users" link="#/users" label="Usuários"/>
+        </nav>
     </aside>
 )
