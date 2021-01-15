6e1448cc26100554abd9614f1b5b55c92c1cbb6b
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-15 10:21:14 -0300

    Component Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './Header.css';
+
+export default props => 
+    (
+        <header className="header">
+            Header
+        </header>
+    )
