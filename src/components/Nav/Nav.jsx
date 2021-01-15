a16fb7ab355b116bd253fd731c5ae15e9aef274e
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-15 10:20:34 -0300

    Components Nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './Nav.css';
+
+
+export default props => (
+    <aside className="menu-area">
+        Nav
+    </aside>
+)
