c2b7ced38ce2953b23caeced518c9d11081b7152
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-15 11:58:49 -0300

    modifying the code

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 
 import Logo from '../components/Logo/Logo';
 import Nav from '../components/Nav/Nav';
-import Main from '../components/Main/Main';
+import Home from '../components/Home/Home';
 import Footer from '../components/Footer/Footer';
 
 export default props => 
@@ -12,7 +14,7 @@ export default props =>
         <div className="app">
             <Logo/>
             <Nav/>
-            <Main/>
+            <Home/>
             <Footer/>
         </div>
     )
