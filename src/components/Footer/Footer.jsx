a6851dde12872329907fcc8b4da0f827327ef296
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-15 10:21:28 -0300

    Component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './Footer.css';
+
+
+export default props => (
+    <footer className="footer">
+        Footer
+    </footer>
+)
