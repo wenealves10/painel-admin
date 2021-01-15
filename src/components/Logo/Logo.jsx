bce7598237de151ff386bab465aacf2cd315a3b1
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-15 12:00:46 -0300

    Logo the code

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Logo.css';
+import Logo from '../../assets/images/blog.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
     (<aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={Logo} alt="logo"/>
+        </a>
     </aside>)
