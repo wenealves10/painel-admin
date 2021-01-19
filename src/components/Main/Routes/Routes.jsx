1be5c94231bf935e187f1e1a968b6fb61f517052
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-19 11:29:25 -0300

    Routes

--- src/components/Main/Routes/Routes.jsx
+++ src/components/Main/Routes/Routes.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router';
+import PrivateRoutes from '../../Auth/PrivateRoutes';
 
 import Home from '../../Home/Home';
+import Login from '../../Login/Login';
 import UserCrud from '../../Users/Users';
 
 export default props => (
     <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/users" component={UserCrud}/>
+        <Route path="/login" component={Login}/>
+        <PrivateRoutes exact path="/" component={Home} />
+        <PrivateRoutes path="/users" component={UserCrud}/>
         <Redirect from="*" to={Home} />
     </Switch>
 )
