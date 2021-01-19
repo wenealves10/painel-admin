7af031f17470c40e48a20f9685e72e0a1520165d
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-19 09:36:28 -0300

    Logo Components

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Logo.css';
 import Logo from '../../assets/images/blog.png';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
     (<aside className="logo">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img src={Logo} alt="logo"/>
-        </a>
+        </Link>
     </aside>)
