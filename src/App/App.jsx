fc018e8d38076754c101ca517ec19cb23c24b849
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-19 09:36:09 -0300

    App Components

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,24 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 
 import Logo from '../components/Logo/Logo';
 import Nav from '../components/Nav/Nav';
-import Home from '../components/Home/Home';
+import Routes from '../components/Main/Routes/Routes';
 import Footer from '../components/Footer/Footer';
 
 export default props => 
     (
-        <div className="app">
-            <Logo/>
-            <Nav/>
-            <Home/>
-            <Footer/>
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <Logo />
+                <Nav />
+                <Routes />
+                <Footer />
+            </div>
+        </BrowserRouter>
     )
